272c9e3dabc329ff40f167c6cdafeeb3d512e616
Ryanhangralim <ryanhangralim@gmail.com>
2024-01-29 13:53:58 +0800

    Update day 8 final project

--- Day_8/final_project.py
+++ Day_8/final_project.py
@@ -1,1 +1,27 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def cipher(mode, message, shift):
+    result = ""
+    for letter in message:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            if mode == 'encode':
+                result += alphabet[index+shift]
+            elif mode == 'decode':
+                result += alphabet[index-shift]
+        else:
+            result += letter
+    return result
+
+
+#menu
+print(logo)
+choice = "yes"
+while choice == "yes":
+    mode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    message = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    print(f"Here's the encoded result: {cipher(mode, message, shift)}")
+    choice = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
