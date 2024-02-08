135393d272fdc46a9c8bb0031298dc5abf2356d1
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-08 11:38:13 +0800

    Init challenge.py

---
+++ Day_18/challenge.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("Black")
+
+#Draw a square
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+
+#Draw dotted line
+#for _ in range(25):
+    # tim.pendown()
+    # tim.forward(10)
+    # tim.penup()
+    # tim.forward(10)
+
+screen = Screen()
+screen.exitonclick()
