82a4d14d8e846e72cc33dd7ca92ec76f0934e944
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-08 11:37:58 +0800

    update

--- Day_18/final_project.py
+++ Day_18/final_project.py
@@ -1,12 +1,9 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("LightSkyBlue")
+tim = Turtle()
+tim.shape("turtle")
+tim.color("Black")
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
 
 
 screen = Screen()
