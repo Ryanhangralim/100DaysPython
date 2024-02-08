6d06d49717f2171a89b2c4662ebcbdf851773fb6
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-08 11:59:52 +0800

    Challenge 4

--- Day_18/challenge.py
+++ Day_18/challenge.py
@@ -5,27 +5,37 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("Black")
 
-#Draw a square
+#Challenge 1 : Draw a square
 # for _ in range(4):
 #     tim.forward(100)
 #     tim.right(90)
 
-#Draw dotted line
+#Challenge 2 : Draw dotted line
 #for _ in range(25):
     # tim.pendown()
     # tim.forward(10)
     # tim.penup()
     # tim.forward(10)
 
-#draw triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
-degree = 360
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-for sides in range(3, 11):
-    angle = degree/sides
-    tim.color(random.choice(colours))
-    for _ in range(sides):
-        tim.forward(100)
-        tim.right(angle)
+#Challenge 3 : draw triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
+# degree = 360
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+# for sides in range(3, 11):
+#     angle = degree/sides
+#     tim.color(random.choice(colours))
+#     for _ in range(sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+#Challenge 4 : random walk
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+# tim.pensize(8)
+# tim.speed("fastest")
+# angles = [0, 90, 180, 270]
+# while True:
+#     tim.color(random.choice(colours))
+#     tim.forward(30)
+#     tim.setheading(random.choice(angles))
 
 screen = Screen()
 screen.exitonclick()
