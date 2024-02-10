ea97a09920bf67ac0a686a792a2dcab8d4bf5417
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-10 15:04:12 +0800

    Update to OOP

--- Day_20/final_project.py
+++ Day_20/final_project.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,30 +8,12 @@ screen.bgcolor('black')
 screen.title("My Snake Game")
 screen.tracer(0)
 
-x_axis = 0
-segments = []
-for _ in range(3):
-    segment = Turtle()
-    segment.shape("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(x_axis, 0)
-    x_axis -= 20
-    segments.append(segment)
-
-screen.update()
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_cor = segments[seg_num-1].pos()
-        segments[seg_num].goto(new_cor)
-    segments[0].forward(20)
-    segments[0].left(90)
-
-
+    snake.move()
 
 screen.exitonclick()
---
+++ Day_20/snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+MOVE_DISTANCE = 20
+
+class Snake():
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+
+    def create_snake(self):
+        self.x_axis = 0
+        for _ in range(3):
+            segment = Turtle()
+            segment.shape("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(self.x_axis, 0)
+            self.x_axis -= 20
+            self.segments.append(segment)
+
+
+    def move(self):
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_cor = self.segments[seg_num-1].pos()
+            self.segments[seg_num].goto(new_cor)
+        self.segments[0].forward(MOVE_DISTANCE)
