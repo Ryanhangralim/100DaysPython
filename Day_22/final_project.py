461202a3f0be701786b9d952f4a871dc084f4266
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-12 12:30:06 +0800

    Add collision with paddle feature

--- Day_22/final_project.py
+++ Day_22/final_project.py
@@ -33,4 +33,13 @@ while game_is_on:
     if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    #detect collision with the wall
+    #detect right paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #detect left paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
