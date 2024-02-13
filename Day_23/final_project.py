b2e947598817596cba4d289d7dff7184f697351d
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-13 10:56:18 +0800

    add turtle and movement

--- Day_23/final_project.py
+++ Day_23/final_project.py
@@ -4,11 +4,24 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+FINISH_LINE_Y = 280
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(key="Up", fun=player.move)
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    #detect collision with finishline
+    if(player.ycor() > FINISH_LINE_Y):
+        player.go_to_start()
+
+screen.exitonclick()
