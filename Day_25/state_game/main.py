021d90eb9e7389fe0b6abc47c92b5b17b34b5365
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-15 15:17:50 +0800

    Add export feature and finalize country

--- Day_25/state_game/main.py
+++ Day_25/state_game/main.py
@@ -2,6 +2,23 @@ import turtle
 from writer import Writer
 import pandas
 
+def export():
+    #gets list of state not answered by the user
+    unanswered_state = {
+        "state" : [],
+        "x" : [],
+        "y" : []
+    }
+    for state in states_list:
+        if state not in answered_state:
+            state_info = data[data["state"] == state]
+            unanswered_state["state"].append(state)
+            unanswered_state["x"].append(state_info.x.iloc[0])
+            unanswered_state["y"].append(state_info.y.iloc[0])
+
+    states = pandas.DataFrame(unanswered_state)
+    states.to_csv("learn.csv")
+
 #screen setup
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -22,10 +39,16 @@ answered_state = []
 
 while len(answered_state) < 50:
     answer_state = screen.textinput(title=f"{len(answered_state)}/50 States Correct", prompt="What's another state's name?")
+
     if answer_state:
         answer_state = answer_state.title()
+
+    if answer_state == "Exit":
+        export()
+        break
+
     #checks if answer is in list
     if((answer_state in states_list) and (answer_state not in answered_state)):
         state_info = data[data["state"] == answer_state]
         writer.write_text(answer_state, state_info.x.iloc[0], state_info.y.iloc[0])
-        answered_state.append(answered_state)
+        answered_state.append(answer_state)
