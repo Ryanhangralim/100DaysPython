94a15ea57d12e07c28901b846789135b3f578ac8
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-16 12:39:04 +0800

    panda iteration done

--- Day_26/panda_iteration.py
+++ Day_26/panda_iteration.py
@@ -7,5 +7,9 @@ import pandas
 
 student_data_frame = pandas.DataFrame(student_dict)
 
-for (key, value) in student_data_frame.items():
+# for (key, value) in student_data_frame.items():
+
+#loops through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(row)
     
