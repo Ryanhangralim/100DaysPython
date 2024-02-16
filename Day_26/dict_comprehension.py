67ea9334fb7fa0460a2e17719def30ff5d2e9239
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-16 12:26:49 +0800

    Init

--- Day_26/dict_comprehension.py
+++ Day_26/dict_comprehension.py
@@ -5,5 +5,5 @@ names = ["Ryan", "Bob", "Pedro", "Walter", "White", "Junior"]
 student_score = {name:random.randint(1, 100) for name in names}
 print(student_score)
 
-passed_students = {name:score for (name,score) in student_score.items() if score > 50}
+passed_students = {name:score for (name, score) in student_score.items() if score > 50}
 print(passed_students)
