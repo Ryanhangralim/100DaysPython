02bbadd5f20a1fcfc2cf87ae92f6a251a1ef93e2
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-17 12:59:20 +0800

    Learning about **

--- Day_27/arguments/main.py
+++ Day_27/arguments/main.py
@@ -6,3 +6,12 @@ def add(*numbers):
     print(total)
 
 add(1,2,3,4,5)
+
+# '**' allows multiple keyword argument to be passed
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
