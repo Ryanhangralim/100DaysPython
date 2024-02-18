2a308682f6b8279d491c6d422f591a9930ce5b8a
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-18 11:20:28 +0800

    Init UI setup

--- Day_28/final_project/main.py
+++ Day_28/final_project/main.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,3 +18,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tk.Canvas(window, width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
