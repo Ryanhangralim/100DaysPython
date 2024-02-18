6f5094638851bb1c3dcadda9ca404c04661f899f
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-18 12:57:16 +0800

    Add countdown and timer mechanism

--- Day_28/final_project/main.py
+++ Day_28/final_project/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from math import floor
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,12 +11,50 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✔"
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label["text"] = "Break"
+        timer_label["fg"] = RED
+    elif reps % 2 == 0  :
+        count_down(short_break_sec)
+        timer_label["text"] = "Break"
+        timer_label["fg"] = PINK
+    else:
+        count_down(work_sec)
+        timer_label["text"] = "Work"
+        timer_label["fg"] = GREEN
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    minutes = floor(count/60)
+    seconds = count % 60
+
+    if seconds < 10:
+        seconds = f"0{seconds}"
+
+    time = f"{minutes}:{seconds}"
+    canvas.itemconfig(timer_text, text=time)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -30,11 +69,11 @@ timer_label.grid(row=0, column=1)
 canvas = tk.Canvas(window, width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 #start button
-start_button = tk.Button(text="Start", highlightthickness=0)
+start_button = tk.Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 #reset button
