e46646dac45ba80eba10402352d3282d19cb549f
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-19 15:07:48 +0800

    Add password row

--- Day_29/main.py
+++ Day_29/main.py
@@ -21,13 +21,23 @@ website = tk.Label(text="Website:")
 website.grid(row=1, column=0)
 
 website_entry = tk.Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2)
 
 #email/username row
 email = tk.Label(text="Email/Username:")
 email.grid(row=2, column=0)
 
 email_entry = tk.Entry(width=35)
-email_entry.grid(row=2, column=1)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+#password row
+password = tk.Label(text="Password:")
+password.grid(row=3, column=0)
+
+password_entry = tk.Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = tk.Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
 
 window.mainloop()
