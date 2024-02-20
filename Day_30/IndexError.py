d69c2e7dc155ce4eceb393ad40323eb5b84e5bf4
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-20 08:45:07 +0800

    Update indexerror.py

--- Day_30/IndexError.py
+++ Day_30/IndexError.py
@@ -3,11 +3,12 @@ fruits = eval(input())
 
 # TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-  try:
-    fruit = fruits[index]
-    print(fruit + " pie")
-  except IndexError:
-    print("Fruit Pie")
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit Pie")
+    else:
+        print(fruit + " pie")
 
 
 # 🚨 Do not change the code below
