dc87c50451d208e555779c3f2d9b4c010b3e424c
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-22 15:07:12 +0800

    Init quotes.txt

---
+++ Day_32/quotes.py
@@ -0,0 +1,16 @@
+import smtplib
+import datetime as dt
+
+#get email and password
+with open("secret.txt", "r") as file:
+    data = file.readlines()
+
+email = data[0]
+password = data[1]
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=email, password=password)
+    connection.sendmail(from_addr=email, 
+                    to_addrs="rh232a@gmail.com", 
+                    msg="Subject:Testing smtp\n\nThis email is for testing smtp purposes")
