17c49c3156fb05b3f0c4b39efc0496b95b2ce83d
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-22 14:52:19 +0800

    Add smtp connection

--- Day_32/main.py
+++ Day_32/main.py
@@ -4,5 +4,11 @@ import smtplib
 with open("secret.txt", "r") as file:
     data = file.readlines()
 
-print(data[0])
-#GMAIL : 100daypythonday32@gmail.com
+email = data[0]
+password = data[1]
+
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
+connection.starttls()
+connection.login(user=email, password=password)
+connection.sendmail(from_addr=email, to_addrs="rh232a@gmail.com", msg="Testing smtp")
+connection.close()
