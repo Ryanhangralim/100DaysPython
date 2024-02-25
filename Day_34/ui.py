3b8240748fd93c5c308523a27e7811ca08b7f0f7
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-25 10:50:49 +0800

    Update question display

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -9,20 +9,24 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        score_board = tk.Label(text="Score: 0", fg="White", background=THEME_COLOR, font=("Arial", 14, "bold"))
-        score_board.grid(row=0, column=1)
+        self.score_board = tk.Label(text="Score: 0", fg="White", background=THEME_COLOR, font=("Arial", 14, "bold"))
+        self.score_board.grid(row=0, column=1)
         
-        question_box = tk.Canvas(width=300, height=250)
-        question_box.grid(row=1, column=0, columnspan=2, pady=20)
+        self.question_box = tk.Canvas(width=300, height=250)
+        self.question_box.grid(row=1, column=0, columnspan=2, pady=20)
 
-        question_text = question_box.create_text(150, 125, text="Question goes here", font=FONT)
+        self.question_text = self.question_box.create_text(150, 125, text="Question goes here", font=FONT)
 
-        checkmark = tk.PhotoImage(file="images/true.png")
-        true_choice = tk.Button(image=checkmark, highlightthickness=0, bd=0)
-        true_choice.grid(row=2, column=0)
+        self.checkmark = tk.PhotoImage(file="images/true.png")
+        self.true_choice = tk.Button(image=self.checkmark, highlightthickness=0, bd=0)
+        self.true_choice.grid(row=2, column=0)
 
-        cross = tk.PhotoImage(file="images/false.png")
-        false_choice = tk.Button(image=cross, highlightthickness=0, bd=0)
-        false_choice.grid(row=2, column=1)
+        self.cross = tk.PhotoImage(file="images/false.png")
+        self.false_choice = tk.Button(image=self.cross, highlightthickness=0, bd=0)
+        self.false_choice.grid(row=2, column=1)
 
         self.window.mainloop()
+
+
+    def next_question(self, question):
+        self.question_box.itemconfig(self.question_text, text=question)
