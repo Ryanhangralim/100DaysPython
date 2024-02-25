27dbe1a79f640560ee7e6c3903e80dcbe8e73100
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-25 10:26:10 +0800

    UI base

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -1,2 +1,10 @@
+import tkinter as tk
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+
+        self.window.mainloop()
