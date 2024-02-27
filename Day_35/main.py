8dedcadc7397bc22d04410860a4e624bbf46f686
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-27 10:31:16 +0800

    Add email sending message

--- Day_35/main.py
+++ Day_35/main.py
@@ -1,9 +1,17 @@
 import requests
+import smtplib
+
+#import email and key
+with open("secret.txt", "r") as file:
+    data = file.readlines()
+
+EMAIL = data[0]
+PASSWORD = data[1]
 
 parameters = {
     "lat" : -8.670458,
     "lon" : 115.212631,
-    "cnt" : 4,
+    "cnt" : 6,
     "appid" : "05085e0a1c7026f86e153ebaed0e3d59"
 }
 
@@ -18,9 +26,15 @@ weather = [weather_code["weather"][0]["id"] for weather_code in weather_data]
 #check if it will rain 
 is_raining = False
 
-for code in weather:
+for code in weather[1:]:
     if code < 700:
         is_raining = True
     
 if is_raining:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=EMAIL, 
+                            to_addrs=EMAIL, 
+                            msg=f"Subject:Rain Alert!\n\nIt will rain today, don't forget to bring an umbrella!")
     print("Bring an umbrella☔")
