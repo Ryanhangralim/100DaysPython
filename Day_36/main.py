0b61fd4f516b44a75f456768eea494003152596c
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-28 12:36:58 +0800

    Add email feature

--- Day_36/main.py
+++ Day_36/main.py
@@ -6,17 +6,31 @@ import datetime
 with open("secret.txt", "r") as file:
     data = file.readlines()
 
+#symbol function
+def symbol(percentage):
+    return "🔺" if percentage > 0 else "🔻"
+
 EMAIL = data[0]
 PASSWORD = data[1]
 AV_APIKEY = data[2]
+NEWS_APIKEY = data[3]
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHAVANTAGE_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines"
 
 ALPHAVANTAGE_PARAMS = {
     "function": "TIME_SERIES_DAILY",
     "symbol" : STOCK,
     "apikey" : AV_APIKEY,
+}
+
+NEWS_PARAMS = {
+    "q" : COMPANY_NAME,
+    "pageSize" : 3,
+    "page" : 1,
+    "apiKey" : NEWS_APIKEY
 }
 
 ## STEP 1: Use https://www.alphavantage.co
@@ -38,6 +52,20 @@ percentage = round(difference/stock_info_2, 5)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+if percentage > 5 or percentage < -5:
+    response = requests.get(url=NEWS_ENDPOINT, params=NEWS_PARAMS)
+    response.raise_for_status()
+    news_response = response.json()["articles"]
+    news_list = [f"{STOCK}: {symbol(percentage).encode('utf-8')}{percentage}% \nHeadline: {news['title'].encode('utf-8')}. \nBrief: {news['description'].encode('utf-8')}." for news in news_response]
+    
+    #sends email
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+
+        for news in news_list:
+            connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL, msg=f"Subject: Stock Update!\n\n{news}")
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
