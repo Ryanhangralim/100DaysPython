eb0211c9402ccc9cbfb18faff2fbd8f216efdd73
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-29 14:03:02 +0800

    Add pixel

--- Day_37/main.py
+++ Day_37/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 USERNAME = "ryanhangralim"
 
@@ -15,11 +16,13 @@ user_params = {
 }
 
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
 
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_ID = "graph1"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Learning Graph",
     "unit": "Min",
     "type": "int",
@@ -30,5 +33,17 @@ headers = {
     "X-USER-TOKEN": token
 }
 
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
-print(response.text)
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+# print(response.text)
+
+now = datetime.datetime.now()
+
+PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_config = {
+    "date" : now.strftime("%Y%m%d"),
+    "quantity" : "60",
+}
+
+# response = requests.post(url=PIXEL_ENDPOINT, json=pixel_config, headers=headers)
+# print(response.text)
