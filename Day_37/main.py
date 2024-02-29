1e6a40b62baf056e69a3197894d8a5156f272836
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-29 13:34:11 +0800

    Read token

--- Day_37/main.py
+++ Day_37/main.py
@@ -1,8 +1,12 @@
 import requests
 
+#get token
+with open("secret.txt", "r") as file:
+    token = file.readline()
+
 ENDPOINT = "https://pixel.la/v1/users"
 user_params = {
-    "token" : "",
+    "token" : token,
     "username" : "RyanHangralim",
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
