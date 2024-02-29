8bef82c76251e980f964456b167a07f686ac06dd
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-29 13:47:09 +0800

    Add graph

--- Day_37/main.py
+++ Day_37/main.py
@@ -15,3 +15,20 @@ user_params = {
 }
 
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Learning Graph",
+    "unit": "Min",
+    "type": "int",
+    "color" : "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": token
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response.text)
