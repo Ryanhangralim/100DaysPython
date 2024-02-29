6f26b1365474645f0a59aaea5c4f3310f0c2a3d1
Ryanhangralim <ryanhangralim@gmail.com>
2024-02-29 13:32:59 +0800

    Add params

---
+++ Day_37/main.py
@@ -0,0 +1,9 @@
+import requests
+
+ENDPOINT = "https://pixel.la/v1/users"
+user_params = {
+    "token" : "",
+    "username" : "RyanHangralim",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
