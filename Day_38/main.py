2dc7c55ea5c7374606bfe8d7ac686d4189ec63b7
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-01 10:09:29 +0800

    Add post api

---
+++ Day_38/main.py
@@ -0,0 +1,31 @@
+import requests
+
+with open("secret.txt", "r") as file:
+    data = file.readlines()
+
+APP_ID = data[0].strip()
+API_KEY = data[1]
+GENDER = "male"
+WEIGHT = 75
+HEIGHT = 178
+AGE = 18
+
+NUTRI_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+nutritionix_headers = {
+    "x-app-id" : APP_ID,
+    "x-app-key" : API_KEY,
+}
+
+exercise_text = input("Tell me which exercise you did: ")
+
+nutritionix_config = {
+    "query" : "swam for 1 hour",
+    "gender" : GENDER,
+    "weight_kg" : WEIGHT,
+    "height_cm" : HEIGHT,
+    "age" : AGE
+}
+
+response = requests.post(url=NUTRI_ENDPOINT, json=nutritionix_config, headers=nutritionix_headers)
+print(response.json())
