09dcd5663f8ab3382d57b7d22c87ac749de03445
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-02 12:17:43 +0800

    Flight search

--- Day_39/flight_search.py
+++ Day_39/flight_search.py
@@ -1,4 +1,5 @@
 import requests
+from flight_data import FlightData
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 
@@ -16,4 +17,41 @@ class FlightSearch:
         response = requests.get(url=location_endpoint, params=query, headers=headers)
         results = response.json()
         return results["locations"][0]["code"]
+    
+    def search_flight(self, origin_city : str, destination_city : str, from_time, to_time, apikey : str):
+        headers = {
+            "apikey" : apikey
+        }
+
+        query = {
+            "fly_from" : origin_city,
+            "fly_to" : destination_city,
+            "date_from" : from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(url=f"{TEQUILA_ENDPOINT}/v2/search", params=query, headers=headers)
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city}.")
+            return None
+        
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
         
--- Day_39/main.py
+++ Day_39/main.py
@@ -1,11 +1,14 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
 with open("secret.txt", "r") as file:
     data = file.readlines()
 
 KIWI_APIKEY = data[0]
+
+ORIGIN_CITY_IATA = "DPS"
 
 sheet_data = DataManager()
 flight_price_data = sheet_data.get_data()
@@ -16,3 +19,15 @@ for city in flight_price_data:
         city["iataCode"] = flight_search.get_iataCode(city=city["city"], apikey=KIWI_APIKEY)
 
 sheet_data.update_data(flight_price_data)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_now = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.search_flight(
+        origin_city=ORIGIN_CITY_IATA,
+        destination_city=destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_now,
+        apikey=KIWI_APIKEY
+    )
