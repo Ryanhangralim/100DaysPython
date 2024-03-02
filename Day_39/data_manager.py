1f2c3ee81a86a6ba8e3e99df073fdd5b9dc07791
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-02 10:53:55 +0800

    Add data_manager.py class

--- Day_39/data_manager.py
+++ Day_39/data_manager.py
@@ -1,3 +1,12 @@
+import requests
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.ENDPOINT = "https://api.sheety.co/cba55caeefe970477a6323961e980c12/flightDeals/prices"
+    
+
+    def get_data(self):
+        response = requests.get(url=self.ENDPOINT)
+        response.raise_for_status()
+        return response.json()["prices"]
