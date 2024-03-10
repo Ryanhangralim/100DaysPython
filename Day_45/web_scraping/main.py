26b91c7cbb1b4e95031ae8adbbc2023c55af05a3
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-10 12:52:40 +0800

    Add web scraping

--- Day_45/web_scraping/main.py
+++ Day_45/web_scraping/main.py
@@ -1,3 +1,36 @@
 import requests
+from bs4 import BeautifulSoup
 
-data = requests.get()
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+article_texts = []
+article_links = []
+article_votes = []
+
+titles = soup.find_all(name="span", class_="titleline")
+
+for title in titles:
+    tag = title.a.text
+    link = title.a.get("href")
+    article_texts.append(tag)
+    article_links.append(link)
+
+subclass = soup.find_all(name="span", class_="score")
+for article in subclass:
+    upvotes = article.text.replace(" points", "")
+    article_votes.append(int(upvotes))
+
+print(article_texts)
+print(article_links)
+print(article_votes)
+max_index = article_votes.index(max(article_votes))
+print(f"Title = {article_texts[max_index]}\nLink = {article_links[max_index]}\nVotes = {article_votes[max_index]}")
+
+
+# titles = soup.select(selector="td .title")
+# print(titles)
+# for title in titles:
+#     print(title.text)
