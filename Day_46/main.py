f999d4728df154ef6445e34a5bd653729d067f93
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-11 11:17:05 +0800

    Song name to spotify link

--- Day_46/main.py
+++ Day_46/main.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 with open("secret.txt", "r") as file:
     data = file.readlines()
 
-CLIENT_ID = data[0]
-CLIENT_SECRET = data[1]
+CLIENT_ID = data[0].strip()
+CLIENT_SECRET = data[1].strip()
 
 # user_year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 user_year = "2018-02-04"
@@ -17,4 +20,20 @@ soup = BeautifulSoup(response, "html.parser")
 
 songs = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
 song_list = [song.text.strip() for song in songs]
-print(song_list)
+
+sp = spotipy.Spotify(
+    auth_manager= SpotifyOAuth(
+        client_id= CLIENT_ID,
+        client_secret= CLIENT_SECRET,
+        redirect_uri= "https://example.com",
+        scope="playlist-modify-private",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+print(sp.current_user()["id"])
+
+# result = sp.search(q="Life will Change", type="track", limit=1)
+# print(result["tracks"]["items"][0]["external_urls"]["spotify"])
+song_links = [sp.search(q=song, type="track", limit=1)["tracks"]["items"][0]["external_urls"]["spotify"] for song in song_list]
+print(song_links)
