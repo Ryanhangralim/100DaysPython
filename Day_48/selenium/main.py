3da211a618e77eb5e471e5243af73dd38c558889
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-26 18:39:30 +0800

    Add by

--- Day_48/selenium/main.py
+++ Day_48/selenium/main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Keeps chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.google.com")
+driver.get("https://www.tokopedia.com/grosirgamebec/game-nintendo-switch-persona-5-royal?extParam=ivf%3Dfalse%26src%3Dsearch")
+
+price = driver.find_element(By.CLASS_NAME, value="price")
+print({price.text})
+
+driver.quit()
