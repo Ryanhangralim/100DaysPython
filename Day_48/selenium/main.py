5eda3ef8b481737eb372c6887e490936e11eb6e7
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-26 17:57:13 +0800

    Add chrome options

--- Day_48/selenium/main.py
+++ Day_48/selenium/main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 
 # Keeps chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.google.com")
