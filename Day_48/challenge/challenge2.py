eb581b5431edd5a5da3a9219f936ed1b75ba93cb
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-26 19:25:35 +0800

    Add 2nd solution to challenge 2

--- Day_48/challenge/challenge2.py
+++ Day_48/challenge/challenge2.py
@@ -11,4 +11,8 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 count = driver.find_element(By.XPATH, value="//*[@id='articlecount']/a[1]")
 print(count.text)
 
+#2nd solution
+article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+print(article_count.text)
+
 driver.quit()
