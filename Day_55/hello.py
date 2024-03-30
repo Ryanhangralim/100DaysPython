16b4f60af782be02b205372d05dd5fc0eb835b3b
Ryanhangralim <ryanhangralim@gmail.com>
2024-03-30 08:38:29 +0800

    Add decorators

--- Day_55/hello.py
+++ Day_55/hello.py
@@ -2,11 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrap():
+        message = function()
+        return f"<b>{message}</b>"
+    return wrap
+
+def make_emphasis(function):
+    def wrap():
+        message = function()
+        return f"<em>{message}</em>"
+    return wrap
+
+def make_underlined(function):
+    def wrap():
+        message = function()
+        return f"<u>{message}</u>"
+    return wrap
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "Bye"
 
@@ -15,4 +36,5 @@ def greet(uname):
     return f"Hello there {uname}"
 
 if __name__ == "__main__":
+    print(say_bye())
     app.run(debug=True)
