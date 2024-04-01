7ec6329ba32a660c353044f1b9da179dcd5f38a1
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-01 17:21:07 +0800

    Add guess feature

--- Day_57/flask/server.py
+++ Day_57/flask/server.py
@@ -11,5 +11,17 @@ def home():
     random_number = random.randint(1, 10)
     return render_template("index.html", num=random_number, current_year=current_year)
 
+@app.route("/guess/<name>")
+def guess(name):
+    AGIFY_ENDPOINT = "https://api.agify.io"
+    GENDERIZE_ENDPOINT = "https://api.genderize.io"
+
+    PARAMS = {
+        "name" : name
+    }
+    agify_response = requests.get(url=AGIFY_ENDPOINT, params=PARAMS).json()
+    genderize_response = requests.get(url=GENDERIZE_ENDPOINT, params=PARAMS).json()
+    return render_template("guess.html", name=name, gender=genderize_response["gender"], age=agify_response["age"])
+
 if __name__ == "__main__":
     app.run(debug=True)
