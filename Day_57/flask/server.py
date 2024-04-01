a9ceda0978400abe18f7d32b0ff22c3ae81e8e4f
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-01 17:33:19 +0800

    Add jinja for loops

--- Day_57/flask/server.py
+++ Day_57/flask/server.py
@@ -23,5 +23,11 @@ def guess(name):
     genderize_response = requests.get(url=GENDERIZE_ENDPOINT, params=PARAMS).json()
     return render_template("guess.html", name=name, gender=genderize_response["gender"], age=agify_response["age"])
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url).json()
+    return render_template("blog.html", posts=response)
+
 if __name__ == "__main__":
     app.run(debug=True)
