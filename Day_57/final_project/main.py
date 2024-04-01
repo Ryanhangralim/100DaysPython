4ea383f9d01143e0395efe2eb6587f585ed8691d
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-01 18:01:34 +0800

    Final project done

--- Day_57/final_project/main.py
+++ Day_57/final_project/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(blog_url).json()
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=response)
+
+@app.route("/post/<page>")
+def get_blog(page):
+    return render_template("post.html", posts=response, page=int(page))
 
 if __name__ == "__main__":
     app.run(debug=True)
