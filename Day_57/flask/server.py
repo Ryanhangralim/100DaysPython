defec6e9849d6367d86a432828848220c660d03f
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-01 17:39:13 +0800

    Add anker tag

--- Day_57/flask/server.py
+++ Day_57/flask/server.py
@@ -24,7 +24,7 @@ def guess(name):
     return render_template("guess.html", name=name, gender=genderize_response["gender"], age=agify_response["age"])
 
 @app.route("/blog")
-def blog():
+def get_blog():
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url).json()
     return render_template("blog.html", posts=response)
