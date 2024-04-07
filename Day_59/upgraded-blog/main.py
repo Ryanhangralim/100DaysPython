0fa5b30865b6ef7e5682adbd0c93f43534f28a3e
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-07 11:36:40 +0800

    Jinja for index.html

--- Day_59/upgraded-blog/main.py
+++ Day_59/upgraded-blog/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(blog_url).json()
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=response)
 
 @app.route("/about")
 def about():
